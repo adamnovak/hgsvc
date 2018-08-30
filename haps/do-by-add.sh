91f60634a268f921b25260ad5170f063797b73e8
Adam Novak <anovak@soe.ucsc.edu>
2018-08-30 09:47:40 -0700

    Make some graph construction steps only run if they aren't run yet

--- haps/do-by-add.sh
+++ haps/do-by-add.sh
@@ -15,27 +15,31 @@ fi
 #chroms=$(for i in $(seq 1 22; echo X; echo Y); do echo chr${i}; done)
 chroms=chr21
 
-echo "constructing HG00514"
-echo $chroms | tr ' ' '\n' | parallel -j $threads "vg construct -r $ref -v ./HGSVC.HG00514.vcf.gz -R {} -C -m 32 -a -f > $base-add0.{}.vg"
+if [[ ! -e $base.xg ]] ; then
+    echo "constructing HG00514"
+    echo $chroms | tr ' ' '\n' | parallel -j $threads "vg construct -r $ref -v ./HGSVC.HG00514.vcf.gz -R {} -C -m 32 -a -f > $base-add0.{}.vg"
 
-echo "adding HG005733"
-echo $chroms | tr ' ' '\n' | parallel -j $threads "bcftools view ./HGSVC.HG005733.vcf.gz -r {} | bgziptabix ./HGSVC.HG005733_{}.vcf.gz; vg add $base-add0.{}.vg -v ./HGSVC.HG005733_{}.vcf.gz > $base-add1.{}.vg ; rm -f ./HGSVC.HG005733_{}.vcf.gz;"
+    echo "adding HG005733"
+    echo $chroms | tr ' ' '\n' | parallel -j $threads "bcftools view ./HGSVC.HG005733.vcf.gz -r {} | bgziptabix ./HGSVC.HG005733_{}.vcf.gz; vg add $base-add0.{}.vg -v ./HGSVC.HG005733_{}.vcf.gz > $base-add1.{}.vg ; rm -f ./HGSVC.HG005733_{}.vcf.gz;"
 
-echo "adding NA19240"
-echo $chroms | tr ' ' '\n' | parallel -j $threads "bcftools view ./HGSVC.NA19240.vcf.gz -r {} | bgziptabix  ./HGSVC.NA19240_{}.vcf.gz; vg add $base-add1.{}.vg -v ./HGSVC.NA19240_{}.vcf.gz > $base.{}.vg ; rm -f ./HGSVC.NA19240_{}.vcf.gz;"
+    echo "adding NA19240"
+    echo $chroms | tr ' ' '\n' | parallel -j $threads "bcftools view ./HGSVC.NA19240.vcf.gz -r {} | bgziptabix  ./HGSVC.NA19240_{}.vcf.gz; vg add $base-add1.{}.vg -v ./HGSVC.NA19240_{}.vcf.gz > $base.{}.vg ; rm -f ./HGSVC.NA19240_{}.vcf.gz;"
 
-echo "node id unification"
-vg ids -j -m $base.mapping $(for i in $chroms; do echo $base.$i.vg; done)
-cp $base.mapping $.base.mapping.backup
+    echo "node id unification"
+    vg ids -j -m $base.mapping $(for i in $chroms; do echo $base.$i.vg; done)
+    cp $base.mapping $.base.mapping.backup
 
-echo "xg indexing"
-vg index -x $base.xg -g $base.gcsa -k 16 -b work -p -t $threads $(for i in $chroms; do echo $base.$i.vg; done)
+    echo "xg indexing"
+    vg index -x $base.xg -g $base.gcsa -k 16 -b work -p -t $threads $(for i in $chroms; do echo $base.$i.vg; done)
+fi    
 
-echo "pruning"
-echo $chroms | tr ' ' '\n' | parallel -j $threads "vg prune -r $base.{}.vg > $base.{}.prune.vg"
+if [[ ! -e $base.gcsa ]] ; then
+    echo "pruning"
+    echo $chroms | tr ' ' '\n' | parallel -j $threads "vg prune -r $base.{}.vg > $base.{}.prune.vg"
 
-echo "gcsa indexing"
-mkdir work
-vg index  -g $base.gcsa -k 16 -b work -p -t $threads $(for i in $chroms; do echo $base.$i.prune.vg; done)
-rm -rf work *.prune.vg
+    echo "gcsa indexing"
+    mkdir work
+    vg index  -g $base.gcsa -Z 4096 -k 16 -b work -p -t $threads $(for i in $chroms; do echo $base.$i.prune.vg; done)
+    rm -rf work *.prune.vg
+fi
 
--- haps/do-by-chrom.sh
+++ haps/do-by-chrom.sh
@@ -15,31 +15,35 @@ fi
 #chroms=$(for i in $(seq 1 22; echo X; echo Y); do echo chr${i}; done)
 chroms=chr21
 
-echo "constructing"
-echo $chroms | tr ' ' '\n' | parallel -j 14 "vg construct -r $ref -v $vars -R {} -C -m 32 -a -f > $base.{}.vg"
+if [[ ! -e $base.threads.xg ]] ; do
+    echo "constructing"
+    echo $chroms | tr ' ' '\n' | parallel -j 14 "vg construct -r $ref -v $vars -R {} -C -m 32 -a -f > $base.{}.vg"
 
-echo "node id unification"
-vg ids -j -m $base.mapping $(for i in $chroms; do echo $base.$i.vg; done)
-cp $base.mapping $base.mapping.backup
+    echo "node id unification"
+    vg ids -j -m $base.mapping $(for i in $chroms; do echo $base.$i.vg; done)
+    cp $base.mapping $base.mapping.backup
 
-echo "indexing haplotypes"
-echo $chroms | tr ' ' '\n' | parallel -j 12 "vg index -x $base.{}.xg -G $base.{}.gbwt -v $vars -F $base.{}.threads $base.{}.vg"
+    echo "indexing haplotypes"
+    echo $chroms | tr ' ' '\n' | parallel -j 12 "vg index -x $base.{}.xg -G $base.{}.gbwt -v $vars -F $base.{}.threads $base.{}.vg"
 
-chrom_count="$(echo $chroms | tr ' ' '\n' | wc -l)"
-if [[ "${chrom_count}" -gt "1" ]] ; then
-    echo "merging GBWT"
-    vg gbwt -m -f -o $base.all.gbwt $(for i in $chroms; do echo $base.$i.gbwt; done)
-else
-    echo "using only gbwt"
-    cp $base.$chroms.gbwt $base.all.gbwt
+    chrom_count="$(echo $chroms | tr ' ' '\n' | wc -l)"
+    if [[ "${chrom_count}" -gt "1" ]] ; then
+        echo "merging GBWT"
+        vg gbwt -m -f -o $base.all.gbwt $(for i in $chroms; do echo $base.$i.gbwt; done)
+    else
+        echo "using only gbwt"
+        cp $base.$chroms.gbwt $base.all.gbwt
+    fi
+
+    echo "extracting threads as paths"
+    for i in $chroms; do ( vg mod $(for f in $(vg paths -L -x $base.$i.xg ); do echo -n ' -r '$f; done) $base.$i.vg; vg paths -x $base.$i.xg -g $base.$i.gbwt -T -V ) | vg view -v - >$base.$i.threads.vg; done
+
+    echo "re-indexing haps+threads"
+    vg index -x $base.threads.xg $(for i in $chroms; do echo $base.$i.threads.vg; done)
+done    
+
+if [[ ! -e  $base.threads.gcsa ]] ; then
+    echo "building gcsa2 index"
+    mkdir -p work
+    TMPDIR=. vg index -g $base.threads.gcsa -Z 4096 -k 16 -p -b work $(for i in $chroms; do echo $base.$i.threads.vg; done)
 fi
-
-echo "extracting threads as paths"
-for i in $chroms; do ( vg mod $(for f in $(vg paths -L -x $base.$i.xg ); do echo -n ' -r '$f; done) $base.$i.vg; vg paths -x $base.$i.xg -g $base.$i.gbwt -T -V ) | vg view -v - >$base.$i.threads.vg; done
-
-echo "re-indexing haps+threads"
-vg index -x $base.threads.xg $(for i in $chroms; do echo $base.$i.threads.vg; done)
-
-echo "building gcsa2 index"
-mkdir -p work
-TMPDIR=. vg index -g $base.threads.gcsa -k 16 -p -b work $(for i in $chroms; do echo $base.$i.threads.vg; done)
